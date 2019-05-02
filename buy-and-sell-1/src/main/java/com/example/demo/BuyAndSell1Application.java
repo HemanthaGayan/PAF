dfad60580d807bfaf4d0691dca221b37e76c322c
Hemantha <Hemantha@DESKTOP-QQ68JME>
2019-05-02 12:12:47 +0530

    first commit

---
+++ buy-and-sell-1/src/main/java/com/example/demo/BuyAndSell1Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuyAndSell1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuyAndSell1Application.class, args);
+	}
+
+}
